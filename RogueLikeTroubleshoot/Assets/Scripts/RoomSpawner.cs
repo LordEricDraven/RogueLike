25da741273b6039359105f899ac1e88085cd2df1
LordEricDraven <c.flamion88@gmail.com>
2019-11-22 14:32:46 -0500

    Update RoomSpawner.cs

--- RogueLikeTroubleshoot/Assets/Scripts/RoomSpawner.cs
+++ RogueLikeTroubleshoot/Assets/Scripts/RoomSpawner.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public int OpeningDirection;
+    // 1 --> need bottom door
+    // 2 --> need top door
+    // 3 --> need left door
+    // 4 --> need right door
+
+
+    private void Update()
     {
-        
+        if(OpeningDirection == 1)
+        {
+            // Need to spawn a room with a BOTTOM door
+        }
+
+        else if(OpeningDirection == 2)
+        {
+            // Need to spawn a room with a TOP door
+        }
+
+        else if(OpeningDirection == 3)
+        {
+            // Need to spawn a room with a LEFT door
+        }
+
+        else if(OpeningDirection == 4)
+        {
+            // Need to spawn a room with a RIGHT door
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
